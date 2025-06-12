8f04111378bf80d99d4eaac23b4bfd2596675696
ayush-trivedi-925 <asxu525@gmail.com>
2025-06-12 11:23:26 +0530

    reorganized qr and mail under services folder for better file structure

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/database/database.module';
 import { AuthController } from './auth.controller';
 import AuthService from './auth.service';
-import { MailModule } from 'src/mail/mail.module';
+import { MailModule } from 'src/services/mail/mail.module';
 
 @Module({
   imports: [DatabaseModule, MailModule],
